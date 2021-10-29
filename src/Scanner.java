4379785c47d23ce8ceda8ca63c8a14e6c4899cd1
Mihai Andrei Popa <popamihai18@yahoo.com>
2021-10-29 18:17:53 +0300

    Lab 5

--- src/Scanner.java
+++ src/Scanner.java
@@ -4,48 +4,79 @@ import java.nio.file.Path;
 import java.util.*;
 import java.util.AbstractMap.SimpleEntry;
 import java.util.Map.Entry;
+import java.util.regex.Pattern;
 
 public class Scanner
 {
     private List<Entry<String, Integer>> pif = new ArrayList<>();
     private SymbolTable st = new SymbolTable();
     private Set<String> tokens = new HashSet<>();
+    private Set<Character> separators = new HashSet<>();
 
     public Scanner() throws IOException
     {
         String tokens_string = Files.readString(Path.of("resources/tokens.in"));
         String[] tokens_split = tokens_string.split("\r\n");
+
         tokens.addAll(Arrays.asList(tokens_split));
+        separators.add(';');
+        separators.add('(');
+        separators.add(')');
+        separators.add(',');
     }
 
     private Entry<String, Integer> detect(String program, Integer index)
     {
         String token = "";
+        Character character = program.charAt(index);
 
-        while (!Character.isWhitespace(program.charAt(index)))
+        if (separators.contains(character))
         {
-            token += program.charAt(index);
-            index++;
+            token += character;
+            return new SimpleEntry<>(token, index);
         }
 
-        return new SimpleEntry<>(token, index);
+        if (character == '"')
+        {
+            do
+            {
+                token += character;
+                index++;
+                character = program.charAt(index);
+            }
+            while (character != '"');
+            token += character;
+            index++;
+        }
+        else
+        {
+            while (!Character.isWhitespace(character) && !separators.contains(character) && index < program.length())
+            {
+                token += character;
+                index++;
+                if (index < program.length())
+                    character = program.charAt(index);
+            }
+        }
+
+        if (token.equals(""))
+            return new SimpleEntry<>(token, index);
+        else
+            return new SimpleEntry<>(token, index - 1);
     }
 
-    private Boolean isIdentifierOrConstant(String raw)
+    private Boolean isIdentifierOrConstant(String token)
     {
-        String token = raw.replace(";", "");
-        if (st.search(token) != -1)
-            return true;
-
-        if (raw.contains(";"))
-            return true;
-
-        return false;
+        return token.equals("true") ||
+                token.equals("false") ||
+                Pattern.matches("[-+]?(0|[1-9][1-90]*)", token) ||
+                Pattern.matches("[A-Za-z][A-Za-z1-90_]*", token) ||
+                Pattern.matches("\"[^\"]*\"", token);
     }
 
     public void scan(String filename) throws IOException
     {
-        String program = Files.readString(Path.of("resources/" + filename));
+        String program = Files.readString(Path.of("resources/" + filename)).replace("\r", "");
         int lineNr = 1;
 
         for (int i = 0; i < program.length(); i++)
@@ -66,12 +97,17 @@ public class Scanner
                 pif.add(new SimpleEntry<>(token, index != -1 ? index : st.add(token)));
             }
             else
+            {
                 System.out.println("Lexical error at line: " + lineNr);
+                break;
+            }
 
             if (program.charAt(i) == '\n')
             {
                 lineNr++;
             }
         }
+
+        System.out.println("Program is lexically correct.");
     }
 }
